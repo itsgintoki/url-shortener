d351106aa485386dfc3e542a1c7e851789d24a9e
itsgintoki <sinhaankit12jan@gmail.com>
2026-06-27 18:06:46 +0530

    fix: cors for production

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json())
 app.use(authenticationMiddleware)
-app.use(cors()) 
+app.use(cors({
+  origin: 'https://url-shortener-gilt-seven.vercel.app'
+}))
 
 // 100 requests per 15 minutes per IP 
 app.use(rateLimit({
